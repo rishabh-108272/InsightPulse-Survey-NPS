406608a3ffd693e109a520813d37bb752194bb86
rishabh-108272 <rishitibest123@gmail.com>
2024-01-18 20:19:33 +0530

    Updated Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Navbar from './navbar/navbar';
-import Survey from './survey';
-const App = () => {
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './navbar/navbar.js';
+import Survey from './survey.js';
+
+function App() {
   return (
-    <div>
-      <Navbar />
-      <Survey/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <Survey /> 
+          </>
+        } />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
